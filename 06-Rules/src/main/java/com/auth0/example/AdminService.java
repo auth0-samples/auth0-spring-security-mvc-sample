eae36c00c6aa12d258e18d1e1fe199484134fa5a
Richard Seldon <arcseldon@gmail.com>
2016-10-21 12:13:39 +0900

    Update auth0-spring-security-mvc dependency to v1.2.1

--- 06-Rules/src/main/java/com/auth0/example/AdminService.java
+++ 06-Rules/src/main/java/com/auth0/example/AdminService.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 /**
  * Demonstration of method level Role based authorization
  * Only an authenticated and authorized User with Admin
@@ -28,6 +30,9 @@ public class AdminService {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         final Auth0UserDetails currentUser = (Auth0UserDetails) authentication.getPrincipal();
         logger.info("Current user accessed Admin secured resource: " + currentUser.getUsername());
+        // add extra info here for retrieval of userMetadata and appMetadata
+        final Map<String, Object> userMetadata = currentUser.getUserMetadata();
+        final Map<String, Object> extraInfo = currentUser.getExtraInfo();
         return true;
     }
 }
