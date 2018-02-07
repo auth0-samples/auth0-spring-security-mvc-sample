10de07cbc7d888f0298fad21c6a698257c1cdb50
Alexis Luque <aluque@indexlabs.com.uy>
2018-02-07 17:14:23 -0300

    Update exec.sh

--- 01-Login/exec.sh
+++ 01-Login/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t auth0-spring-security-mvc-01-login .
-docker run -p 3000:3000 -v ~/.m2:/home/gradle/.gradle -it auth0-spring-security-mvc-01-login
+docker build -t auth0-samples/auth0-spring-security-mvc-01-login .
+docker run -p 3000:3000 -it auth0-samples/auth0-spring-security-mvc-01-login
